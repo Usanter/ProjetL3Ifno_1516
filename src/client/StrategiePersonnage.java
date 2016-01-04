c1623df8018dcedea6df985d545451618800bef1
Usanter <TRolland@protonmail.ch>
2016-01-04 21:04:35 +0100

    Ajout StratVoleur et modification StatPerso

--- src/client/StrategiePersonnage.java
+++ src/client/StrategiePersonnage.java
@@ -18,14 +18,14 @@ import utilitaires.Constantes;
 /**
  * Strategie d'un personnage. 
  */
-public class StrategiePersonnage {
+public abstract class StrategiePersonnage {
 	
 	/**
 	 * Console permettant d'ajouter une phrase et de recuperer le serveur 
 	 * (l'arene).
 	 */
 	protected Console console;
-
+	protected int type ;
 	/**
 	 * Cree un personnage, la console associe et sa strategie.
 	 * @param ipArene ip de communication avec l'arene
@@ -39,8 +39,8 @@ public class StrategiePersonnage {
 	 */
 	public StrategiePersonnage(String ipArene, int port, String ipConsole, 
 			String nom, String groupe, HashMap<Caracteristique, Integer> caracts,
-			long nbTours, Point position, LoggerProjet logger) {
-		
+			long nbTours, Point position, LoggerProjet logger ) {
+		this.type = type;
 		logger.info("lanceur", "Creation de la console...");
 		
 		try {
@@ -63,59 +63,8 @@ public class StrategiePersonnage {
 	 * @param voisins element voisins de cet element (elements qu'il voit)
 	 * @throws RemoteException
 	 */
-	public void strategie(HashMap<Integer, Point> voisins) throws RemoteException {
-		// arene
-		IArene arene = console.getArene();
-		
-		// reference RMI de l'element courant
-		int refRMI = 0;
-		
-		// position de l'element courant
-		Point position = null;
-		
-		try {
-			refRMI = console.getRefRMI();
-			position = arene.getPosition(refRMI);
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		}
-		
-		if (voisins.isEmpty()) { // je n'ai pas de voisins, j'erre
-			console.setPhrase("J'erre...");
-			arene.modifCara(refRMI, 1 , Caracteristique.POUVOIR);
-			// a modifier pour chaque personnage ( temps de recharge des pouvoirs )
-			if(console.getPersonnage().getCaract(Caracteristique.POUVOIR) > 10){
-				arene.modifCara(refRMI, -console.getPersonnage().getCaract(Caracteristique.POUVOIR), 	Caracteristique.POUVOIR);
-			}
-			
-			
-			arene.deplace(refRMI, 0); 
-		} else {
-			int refCible = Calculs.chercherElementProche(position, voisins);
-			int distPlusProche = Calculs.distanceChebyshev(position, arene.getPosition(refCible));
+	public abstract void strategie(HashMap<Integer, Point> voisins) throws RemoteException ;
 
-			Element elemPlusProche = arene.elementFromRef(refCible);
-
-			if(distPlusProche <= Constantes.DISTANCE_MIN_INTERACTION) { // si suffisamment proches
-				// j'interagis directement
-				if(elemPlusProche instanceof Potion) { // potion
-					// ramassage
-					console.setPhrase("Je ramasse une potion");
-					arene.ramassePotion(refRMI, refCible);
-
-				} else { // personnage
-					// duel
-					console.setPhrase("Je fais un duel avec " + elemPlusProche.getNom());
-					arene.lanceAttaque(refRMI, refCible);
-				}
-				
-			} else { // si voisins, mais plus eloignes
-				// je vais vers le plus proche
-				console.setPhrase("Je vais vers mon voisin " + elemPlusProche.getNom());
-				arene.deplace(refRMI, refCible);
-			}
-		}
-	}
 
 	
 }
